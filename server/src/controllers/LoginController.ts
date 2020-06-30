379490ee2ca57c9437d30e470ef2a1b8e83a5119
deivezera <dzabroski@gmail.com>
2020-06-29 23:12:01 -0300

    Formato do login

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -3,6 +3,14 @@ import knex from '../database/connection'
 
 class ServiceOrderController {
     async login(req: Request, res: Response) {
+        //req.body = login/email, senha
+        // senha vai virar hash
+        // email/ senha vai ser usado como parametro de busca no banco de dados para checar se existe aquele usuário
+        const token = {
+            token: "",
+            expire: ""
+        }
+        //gerar token e data de expiração do token +o- 10min de expiração
         return
     }
 }
